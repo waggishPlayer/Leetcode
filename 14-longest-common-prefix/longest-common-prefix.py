bbe32ecb9543a4cb8fc2e32a5024e70f30c36307
Ali Badshah <ali.accessideas@gmail.com>
2026-05-05 20:47:43 +0530

    Time: 3 ms (30.10%) | Memory: 12.4 MB (69.36%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,18 +1,9 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        min_len = min(len(word) for word in strs)
         prefix = ""
-
-        for i in range(min_len):
-            check = strs[0][i]
-
-            for word in strs:
-                if check != word[i]:
-                    return prefix
-            
-            prefix += strs[0][i]
+        for column in zip(*strs):
+            if len(set(column)) == 1:
+                prefix += column[0]
+            else:
+                break
         return prefix
