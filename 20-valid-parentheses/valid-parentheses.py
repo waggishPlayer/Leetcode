ce22e16e320d8c1a98eb17c2fb07e311fbd153c8
Ali Badshah <ali.accessideas@gmail.com>
2026-05-11 12:32:07 +0530

    Time: 4 ms (41.92%) | Memory: 12.8 MB (7.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                stack.append(s[i])
+            else:
+                if s[i] == ')':
+                    if len(stack) > 0 and '(' == stack[-1]:
+                        stack.pop()
+                    else:
+                        return False
+                if s[i] == '}':
+                    if len(stack) > 0 and '{' == stack[-1]:
+                        stack.pop()
+                    else:
+                        return False
+                if s[i] == ']':
+                    if len(stack) > 0 and '[' == stack[-1]:
+                        stack.pop()
+                    else:
+                        return False
+        
+        if len(stack) == 0:
+            return True
+        else:
+            return False
