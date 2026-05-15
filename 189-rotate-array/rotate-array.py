0a3614895e511c2175361c94b10030cb0c2ea800
Ali Badshah <ali.accessideas@gmail.com>
2026-05-15 12:29:18 +0530

    Time: 3 ms (74.48%) | Memory: 22.3 MB (62.56%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if k > len(nums):
+            k = k%len(nums)
+        nums.reverse()
+
+        self.rev_chunk(nums, 0, k-1)
+        self.rev_chunk(nums, k, len(nums)-1)
+
+        return nums
+
+    def rev_chunk(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
