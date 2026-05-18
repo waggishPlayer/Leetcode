0aa2d4d06479a8778cbcbf7c1738da489aa9eed5
Ali Badshah <ali.accessideas@gmail.com>
2026-05-18 20:31:10 +0530

    Time: 15 ms (87.98%) | Memory: 12.7 MB (49.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,23 +5,25 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        
         count = {}
 
         if len(s) != len(t):
             return False
-
+        
         for char in s:
             if char not in count:
                 count[char] = 1
             else:
                 count[char] += 1
-
+        
         for char in t:
             if char not in count:
                 return False
-            
-            count[char] -= 1
-            if count[char] < 0:
-                return False
+            else:
+                count[char] -= 1
+                if count[char] < 0:
+                    return False
+        
 
         return True
