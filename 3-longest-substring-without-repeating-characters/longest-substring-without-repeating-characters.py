73371087e706f0bb528da673b3a510a04652a689
Ali Badshah <ali.accessideas@gmail.com>
2026-05-22 15:44:30 +0530

    Time: 26 ms (47.29%) | Memory: 12.5 MB (80.85%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,25 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
-        if len(s) == 1:
-            return 1
-
-        right = 0
         left = 0
-        max_num = 0
+        right = 0
+        cur_sub = 0
+        longest_sub = 0
         window = set()
 
         while right < len(s):
-            if s[right] in window:
-                window.remove(s[left])
-                left += 1
-
-            else:
+            if s[right] not in window:
                 window.add(s[right])
-                right += 1
-                
-            max_num = max(max_num, right - left)
-        
-        return max_num
-        
+                right +=1
+            else:
+                window.remove(s[left])
+                left +=1
+
+            cur_sub = right - left
+            longest_sub = max(longest_sub, cur_sub)
+
+        return longest_sub  
