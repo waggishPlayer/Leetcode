40be9e6384fbd0dada0d55753ea78fefb36ab331
Ali Badshah <ali.accessideas@gmail.com>
2026-08-28 11:50:30 +0530

    Time: 35 ms (90.76%) | Memory: 21.2 MB (44.10%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,17 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        max_sum = -99999
+        window = 0
+        left = 0
+
+        for right in range(len(nums)):
+            window += nums[right]
+            if window > max_sum:
+                max_sum = window
+
+            if window < 0:
+                window  = 0
+                left += 1
         
-        cur = 0
-        s = nums[0]
+        return max_sum
 
-        for i in range(len(nums)):
-            cur += nums[i]
 
-            if cur > s:
-                s = cur
-
-            if cur < 0:
-                cur = 0
-            
-        return s
+        
