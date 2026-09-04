3a6980efeb8725333c6e0f16cbaa9ca76b6a8ad3
Ali Badshah <ali.accessideas@gmail.com>
2026-09-04 07:59:20 +0530

    Time: 419 ms (33.46%) | Memory: 16.9 MB (7.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,20 +5,17 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        right = 0
-        cur_sub = 0
-        longest_sub = 0
-        window = set()
+        store = set()
+        count = 0
 
-        while right < len(s):
-            if s[right] not in window:
-                window.add(s[right])
-                right +=1
-            else:
-                window.remove(s[left])
-                left +=1
+        for right in range(len(s)):
+            while s[right] in store:
+                store.remove(s[left])
+                left += 1
+                
 
-            cur_sub = right - left
-            longest_sub = max(longest_sub, cur_sub)
+            store.add(s[right])
+            count = max(count, right - left + 1)
+                
 
-        return longest_sub  
+        return count
